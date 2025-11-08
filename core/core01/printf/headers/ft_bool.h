79ff2d5dfa066bf1e58cb7d6e34a75da40e2c189
Swann Deppe <sdeppe@student.42heilbronn.de>
2025-11-08 18:48:22 +0100

    simp preparation

---
+++ core/core01/printf/headers/ft_bool.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bool.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdeppe <sdeppe@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 12:30:56 by sdeppe            #+#    #+#             */
+/*   Updated: 2025/11/08 12:58:11 by sdeppe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOL_H
+# define FT_BOOL_H
+
+# if !defined(__cplusplus) && !defined(__STDBOOL_H)
+#  define bool char
+#  define false 0
+#  define true 1
+# endif
+
+#endif
